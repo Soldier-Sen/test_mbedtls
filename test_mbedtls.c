065c189392081c4cbe8873b71c91f08ce7ca0a86
ancloudtech_1319369427 <1319369427@qq.com>
2018-12-23 16:29:21 +0800

    1.增加aes-cbc-256加密测试代码; 2.增加aes-ecb加密测试代码; 3.增加md5校验代码，支持求字符串和求文件的md5值

--- test_mbedtls.c
+++ test_mbedtls.c
@@ -3,29 +3,32 @@
 #include <unistd.h>
 #include <strings.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <ctype.h>
 #include <getopt.h>
 
 #include <mbedtls/config.h>
 #include <mbedtls/aes.h>
+#include <mbedtls/md5.h>
+
 
 #define NO_ARG				0
 #define HAS_ARG				1
 
 static struct option long_options[] = {
-	{"aes", HAS_ARG, 0, 'a'},
-	{"function", HAS_ARG, 0, 'f'},
-	{"serialNumber", NO_ARG, 0, 's'},
-	{"macAddress", NO_ARG, 0, 'm'},
+	{"mode", HAS_ARG, 0, 'm'},
+	{"file", HAS_ARG, 0, 'f'},
+	{"string", HAS_ARG, 0, 's'},
 	{0, 0, 0, 0}
 };
 
-static const char *short_options = "Vvf:MmSs";
+static const char *short_options = "f:m:s:";
 
 void usage(void)
 {
 	int i;
 
-	printf("\mbedtls usage:\n");
+	printf("\nmbedtls usage:\n");
 	for (i = 0; i < sizeof(long_options) / sizeof(long_options[0]) - 1; i++) 
 	{
 		if (isalpha(long_options[i].val))
@@ -39,7 +42,9 @@ void usage(void)
 }
 
 
-#define AES_KEY_LEN  16
+#define AES_128_KEY_LEN  16
+#define AES_256_KEY_LEN  32
+
 // key: 35408382fef5a3decf784f74d3f1d97e
 int load_key(const char *keyFile, unsigned char *key, int keyLen);
 int load_file_data(const char *fileName, unsigned char **data, int *data_len);
@@ -47,16 +52,18 @@ int save_file(const char *path, const unsigned char *data, unsigned int size);
 
 int main(int argc, char *argv[])
 {
-	int cbc_result = -1;
+	int result = -1;
 	unsigned char iv[16] = {0};
-	char msg[16+1] = "0123456789abcdef";
+	//char msg[16+1] = "0123456789abcd";
+	char msg[16+1] = {0};
+	char buf[4096] = {0};
 	char enc_buf[16+1] = {0};
 	char dec_buf[16+1] = {0};
-	char fileName[16] = "Makefile";
+	char fileName[64] = "";
+	struct timeval start, end;
 	
-	unsigned char key[AES_KEY_LEN] = {0};
 	mbedtls_aes_context ctx;
-	char aes_mode[8] = {0};
+	char mode[8] = {0};
 	
 	int ch;
 	int option_index = 0;
@@ -66,23 +73,15 @@ int main(int argc, char *argv[])
 		switch (ch) 
 		{
 			case 'f':
-				printf("optind = %d, optarg = %s\n", optind, optarg);
+				strncpy(fileName, optarg, sizeof(fileName) - 1);
+				printf("optind = %d, optarg = %s, fileName = %s\n", optind, optarg, fileName);
 				break;
-			case 'v':
-			//version_flag = 1;
-				break;
-
 			case 's':
-			//serialNumber_flag = 1;
+				strncpy(msg, optarg, sizeof(msg) - 1);
 				break;
-
-			case 'M':
 			case 'm':
-			//macAddress_flag = 1;
-				break;
-			case 'a':
-				strcpy(aes_mode, optarg);
-				printf("aes_mode = %s\n", aes_mode);
+				strcpy(mode, optarg);
+				printf("mode = %s\n", mode);
 			//all_flag = 1;
 				break;
 
@@ -92,26 +91,108 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	
 	mbedtls_aes_init(&ctx);
 
-	if(strcmp(aes_mode, "cbc-128") == 0)
+	if(strcmp(mode, "cbc-128") == 0)
 	{
+		unsigned char key[AES_128_KEY_LEN] = {0};
 		char key_file_128[16] = "aes128.key";
 		load_key(key_file_128, key, sizeof(key));
-		mbedtls_aes_setkey_enc(&ctx, key, AES_KEY_LEN*8);
+		mbedtls_aes_setkey_enc(&ctx, key, AES_128_KEY_LEN*8);
 		
 		int len = sizeof(msg) - 1;
-		cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_ENCRYPT, len, iv, msg, enc_buf);
-		//cbc_result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_ENCRYPT,msg, enc_buf);
-		printf("ENC: cbc_result = %d, msg:[%s] -> dec_buf:[%s]\n",cbc_result, msg, enc_buf);
+		result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_ENCRYPT, len, iv, msg, enc_buf);
+		//result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_ENCRYPT,msg, enc_buf);
+		printf("ENC: cbc_result = %d, 明文msg:[%s] -> 密文dec_buf:[%s]\n",result, msg, enc_buf);
 		
 		memset(iv, 0x0, sizeof(iv));
-		mbedtls_aes_setkey_dec(&ctx, key, AES_KEY_LEN*8);
-		cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_DECRYPT, len, iv, enc_buf, dec_buf);
-		//cbc_result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_DECRYPT,msg, enc_buf);
-		printf("DEC: cbc_result = %d, [%s] -> dec = [%s]\n",cbc_result, enc_buf, dec_buf);
+		mbedtls_aes_setkey_dec(&ctx, key, AES_128_KEY_LEN*8);
+		result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_DECRYPT, len, iv, enc_buf, dec_buf);
+		//result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_DECRYPT,msg, enc_buf);
+		printf("DEC: cbc_result = %d, [%s] -> dec = [%s]\n",result, enc_buf, dec_buf);
 
+	}
+	else if(strcmp(mode, "cbc-256") == 0)
+	{
+		unsigned char key[AES_256_KEY_LEN] = {0};
+		char key_file_256[16] = "aes256.key";
+		load_key(key_file_256, key, sizeof(key));
+		mbedtls_aes_setkey_enc(&ctx, key, AES_256_KEY_LEN*8);
+		
+		int len = sizeof(msg) - 1;
+		result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_ENCRYPT, len, iv, msg, enc_buf);
+		//result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_ENCRYPT,msg, enc_buf);
+		printf("ENC: cbc_result = %d, 明文msg:[%s] -> 密文dec_buf:[%s]\n",result, msg, enc_buf);
+		
+		memset(iv, 0x0, sizeof(iv));
+		mbedtls_aes_setkey_dec(&ctx, key, AES_256_KEY_LEN*8);
+		result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_DECRYPT, len, iv, enc_buf, dec_buf);
+		//result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_DECRYPT,msg, enc_buf);
+		printf("DEC: 256 cbc_result = %d, [%s] -> dec = [%s]\n",result, enc_buf, dec_buf);
+	}
+	else if(strcmp(mode, "ecb") == 0)
+	{
+		//密钥长度可以选择256
+		unsigned char key[AES_128_KEY_LEN] = {0};
+		char key_file_128[16] = "aes128.key";
+		load_key(key_file_128, key, sizeof(key));
+		//加密
+		mbedtls_aes_setkey_enc(&ctx, key, AES_128_KEY_LEN*8);
+		result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_ENCRYPT, msg, enc_buf);
+		printf("ENC: ecb result = %d, 明文msg:[%s] -> 密文dec_buf:[%s]\n",result, msg, enc_buf);
+		//解密
+		mbedtls_aes_setkey_dec(&ctx, key, AES_128_KEY_LEN*8);
+		result = mbedtls_aes_crypt_ecb(&ctx, MBEDTLS_AES_DECRYPT, enc_buf, dec_buf);
+		printf("DEC: ecb result = %d, [%s] -> dec = [%s]\n",result, enc_buf, dec_buf);
+	}
+	else if(strcmp(mode, "md5") == 0)
+	{
+		unsigned char digest[16] = {0};
+		mbedtls_md5_context md5_ctx;
+
+		//求文件的MD5 值
+		if(strlen(fileName) > 0)
+		{
+			int read_len = 0;
+			FILE *fp = fopen(fileName, "rb");
+			if(!fp){printf("fopen %s fail !", fileName);return -1;}
+
+			gettimeofday(&start, NULL);
+			mbedtls_md5_init(&md5_ctx);
+			mbedtls_md5_starts_ret(&md5_ctx);
+			while((read_len = fread(buf, 1, sizeof(buf), fp)) > 0){
+				//printf("read_len = %d\n", read_len);
+				mbedtls_md5_update_ret(&md5_ctx, buf, read_len);
+			}
+			mbedtls_md5_finish_ret(&md5_ctx, digest);
+			gettimeofday(&end, NULL);
+			fclose(fp);
+			mbedtls_md5_free(&md5_ctx);
+			float use_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec -start.tv_usec)/1000.0;
+			printf("file %s use time: %.4f ms, digest:", fileName, use_time);
+			for(int i = 0; i< sizeof(digest); i++)
+			{
+				 printf("%02x", digest[i]);
+			}
+			printf("\n");
+		}
+		//求指定字符串的MD5 值
+		if(strlen(msg) > 0)
+		{
+			mbedtls_md5_init(&md5_ctx);
+			mbedtls_md5_starts_ret(&md5_ctx);
+
+			mbedtls_md5_update_ret(&md5_ctx, msg, strlen(msg));
+			mbedtls_md5_finish_ret(&md5_ctx, digest);
+			mbedtls_md5_free(&md5_ctx);
+			printf("string %s digest:", msg);
+			for(int i = 0; i< sizeof(digest); i++)
+			{
+				 printf("%02x", digest[i]);
+			}
+			printf("\n");
+		}
+		
 	}
 
 	
